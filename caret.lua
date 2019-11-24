9cba961c86f05997a2f776a9e5790940dbee73c5
pietvanzoen <hi@pietvanzoen.com>
2019-11-24 19:09:34 +0100

    initial version

---
+++ caret.lua
@@ -0,0 +1,43 @@
+local Caret = {
+  width = 10,
+  height = 10,
+  angle = 0,
+  speed = 2,
+  x = 0,
+  y = 0,
+}
+
+function Caret:new()
+  newObj = { }
+  self.__index = self
+  return setmetatable(newObj, self)
+end
+
+function Caret:points()
+  local x1 = (self.width / 2)
+  local y1 = 0
+  local x2 = self.width
+  local y2 = self.height
+  local x3 = 0
+  local y3 = self.height
+  return {x1, y1, x2, y2, x3, y3}
+end
+
+function Caret:moveRight()
+  self.angle = 1.57
+  self.x = self.x + self.speed
+end
+function Caret:moveLeft()
+  self.angle = 4.71
+  self.x = self.x - self.speed
+end
+function Caret:moveUp()
+  self.angle = 0
+  self.y = self.y - self.speed
+end
+function Caret:moveDown()
+  self.angle = 3.14
+  self.y = self.y + self.speed
+end
+
+return Caret
---
+++ main.lua
@@ -0,0 +1,33 @@
+Caret = require('caret')
+
+function love.load()
+  caret = Caret:new()
+  caretCanvas = love.graphics.newCanvas(caret.width, caret.height)
+  love.graphics.setCanvas(caretCanvas)
+    love.graphics.clear()
+    love.graphics.polygon('fill', caret:points())
+  love.graphics.setCanvas()
+end
+
+function love.draw()
+  local scale = 1
+  local centerOffsetX = caret.width / 2
+  local centerOffsetY = caret.height / 2
+  love.graphics.draw(caretCanvas, caret.x, caret.y, caret.angle, scale, scale, centerOffsetX, centerOffsetY)
+  love.graphics.print('x: '.. caret.x .. ', y: ' .. caret.y, 5, 5)
+end
+
+function love.update(dt)
+   if love.keyboard.isDown("right") then
+     caret:moveRight()
+   end
+   if love.keyboard.isDown("left") then
+     caret:moveLeft()
+   end
+   if love.keyboard.isDown("up") then
+     caret:moveUp()
+   end
+   if love.keyboard.isDown("down") then
+     caret:moveDown()
+   end
+end
